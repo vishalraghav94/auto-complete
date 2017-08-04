79d9405adeb55b900851d579c5c9a3079afc922b
Vishal Raghav <vishal.raghav@wingify.com>
2017-08-04 16:34:19 +0530

    added automatic completion without pressing any button

--- js/autoCompleteDirective.js
+++ js/autoCompleteDirective.js
@@ -7,7 +7,20 @@ myApp.directive('autoComp',[ function () {
         templateUrl: 'option-list.html',
         link: function (scope, element, event) {
                 scope.listVisible = false;
-                scope.dictionary = ["The", "String", "object", "lets", "you", "work", "with", "a", "series", "of", "characters.", "It", "wraps", "the", "string", "primitive", "data", "type", "with", "a", "number", "of", "helper", "methods"];//['hello', 'hell', 'gaurav', 'ayushi', 'search','now', 'new'];
+                scope.dictionary = ["The", "String", "object", "lets",
+                    "you", "work", "with", "a", "series", "of", "characters.",
+                    "It", "wraps", "the", "string", "primitive", "data", "type",
+                    "with", "a", "number", "of", "helper", "methods",
+                    "Typical", "implementations", "of", "A*", "use", "a", "priority",
+                    "queue", "to", "perform", "the", "repeated", "selection", "of", "minimum", "(estimated)",
+                    "cost", "nodes", "to", "expand.", "This", "priority", "queue", "is", "known", "as", "the",
+                    "open", "set", "or", "fringe.", "At", "each", "step", "of", "the", "algorithm,", "the",
+                    "node", "with", "the", "lowest", "f(x)", "value", "is", "removed", "from", "the", "queue,",
+                    "the", "f", "and", "g", "values", "of", "its", "neighbors", "are", "updated",
+                    "accordingly,", "and", "these", "neighbors", "are", "added", "to", "the", "queue.",
+                    "The", "algorithm", "continues", "until", "a", "goal", "node", "has", "a", "lower", "f",
+                    "value", "than", "any", "node", "in", "the", "queue", "(or", "until", "the", "queue", "is",
+                    "empty).[a]", "The", "f", "value", "of", "the", "goal", "is", "then"];//['hello', 'hell', 'gaurav', 'ayushi', 'search','now', 'new'];
                scope.makeSuggestions = function () {
                    scope.suggestions = scope.dictionary.filter(function (ele, index) {
                        return !ele.indexOf(scope.pattern + "") && (index === scope.dictionary.indexOf(ele));
@@ -19,19 +32,36 @@ myApp.directive('autoComp',[ function () {
                };
                scope.selectedIndex = 0;
                scope.autoComplete = function (event) {
-                   if (event.key === 'Enter') {
-                       if(scope.suggestions) {
+                   if (event.key === "Backspace") {
+                       scope.selectedIndex = 0;
+                   }
+                   if ((event.key !== 'Enter') && (event.key !== 'ArrowDown') && (event.key !== 'ArrowUp') ) {
+
+                   }
+                   else {
+                       if (!scope.pattern) {
+                           scope.selectedIndex = 0;
+                       }
+                       if (event.key === 'ArrowDown') {
+                           if (scope.selectedIndex < scope.suggestions.length - 1) {
+                               scope.selectedIndex++;
+                           }
+                       }
+                       if (event.key === 'ArrowUp') {
+                           if (scope.selectedIndex > 0) {
+                               scope.selectedIndex--;
+                           }
+                       }
+                       if (scope.suggestions) {
                            scope.pattern = scope.suggestions[scope.selectedIndex];
+
                        }
-                   }
-                   else if (event.key === 'ArrowDown') {
-                       if (scope.selectedIndex < scope.suggestions.length - 1) {
-                           scope.selectedIndex++;
-                       }
-                   }
-                   else if (event.key === 'ArrowUp') {
-                       if (scope.selectedIndex > 0) {
-                           scope.selectedIndex--;
+                       if (event.key === 'Enter') {
+                           if (scope.suggestions) {
+                               scope.pattern = scope.suggestions[scope.selectedIndex];
+                               scope.suggestions = undefined;
+                               scope.selectedIndex = 0;
+                           }
                        }
                    }
                }
